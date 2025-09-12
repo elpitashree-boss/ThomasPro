d264147de967e0485ee535e46050d83e9ba48fe7
elpitashree-boss <elpitashree@gmail.com>
2025-09-12 07:13:55 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -271,7 +271,8 @@ Nᴀᴍᴇ - {}"""
 <blockquote>👑 <b>Oᴡɴᴇʀ :</b> <a href='http://t.me/EL_Pita_Shree'>𝔾𝖍𝖔𝖘𝖙 ♞</a></blockquote>
 
     ALL_FILTERS = """
-<blockquote>These are my three types of filters.</blockquote>
+<blockquote>Here are the three types of filters available.</blockquote>
+"""
     
     GFILTER_TXT = """
 <b>Wᴇʟᴄᴏᴍᴇ ᴛᴏ Gʟᴏʙᴀʟ Fɪʟᴛᴇʀs. Gʟᴏʙᴀʟ Fɪʟᴛᴇʀs ᴀʀᴇ ᴛʜᴇ ғɪʟᴛᴇʀs sᴇᴛ ʙʏ ʙᴏᴛ ᴀᴅᴍɪɴs ᴡʜɪᴄʜ ᴡɪʟʟ ᴡᴏʀᴋ ᴏɴ ᴀʟʟ ɢʀᴏᴜᴘs.</b>
