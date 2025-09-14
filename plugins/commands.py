921e5de0f8c3299f31811bf15ec651788050e1a9
elpitashree-boss <elpitashree@gmail.com>
2025-09-14 18:36:36 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -88,11 +88,11 @@ async def start(client, message):
         return
 
     if AUTH_CHANNEL:
-    not_joined = []
-    for channel in AUTH_CHANNEL:
-        try:
-            member = await client.get_chat_member(channel, message.from_user.id)
-            if member.status not in ("member", "administrator", "creator"):
+        not_joined = []
+        for channel in AUTH_CHANNEL:
+            try:
+                member = await client.get_chat_member(channel, message.from_user.id)
+                if member.status not in ("member", "administrator", "creator"):
                 not_joined.append(channel)
         except:
             not_joined.append(channel)
