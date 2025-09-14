fc97387cd74983957ed56e3e51297dee2a77f2ed
elpitashree-boss <elpitashree@gmail.com>
2025-09-14 09:30:29 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -87,10 +87,36 @@ async def start(client, message):
         )
         return
     
-    if AUTH_CHANNEL and not await is_subscribed(client, message):
+    if AUTH_CHANNEL:
+    not_joined = []
+    for channel in AUTH_CHANNEL:
         try:
-            if REQUEST_TO_JOIN_MODE == True:
-                invite_link = await client.create_chat_invite_link(chat_id=(int(AUTH_CHANNEL)), creates_join_request=True)
+            member = await client.get_chat_member(channel, message.from_user.id)
+            if member.status not in ("member", "administrator", "creator"):
+                not_joined.append(channel)
+        except:
+            not_joined.append(channel)
+
+    if not_joined:
+        buttons = []
+        for ch in not_joined:
+            try:
+                if REQUEST_TO_JOIN_MODE == True:
+                    invite_link = await client.create_chat_invite_link(chat_id=ch, creates_join_request=True)
+                else:
+                    invite_link = await client.create_chat_invite_link(ch)
+                buttons.append([InlineKeyboardButton("📢 Join Channel", url=invite_link.invite_link)])
+            except Exception as e:
+                print(e)
+
+        if message.command[1] != "subscribe":
+            buttons.append([InlineKeyboardButton("↻ Try Again", url=f"https://t.me/{temp.U_NAME}?start={message.command[1]}")])
+
+        await message.reply_text(
+            "<blockquote>🚨 Access Restricted!\n\n✨ To unlock premium features, please join all the required channels below 👇</blockquote>",
+            reply_markup=InlineKeyboardMarkup(buttons)
+        )
+        return
             else:
                 invite_link = await client.create_chat_invite_link(int(AUTH_CHANNEL))
         except Exception as e:
