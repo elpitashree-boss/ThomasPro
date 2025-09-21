397cf3d935aa4a6ea18c86817ad6f3713223d91a
elpitashree-boss <elpitashree@gmail.com>
2025-09-21 08:34:26 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -39,7 +39,7 @@ async def start(client, message):
 ],[
     InlineKeyboardButton('Dᴇᴠᴇʟᴏᴘᴇʀ ☠️', url='https://t.me/EL_Pita_Shree')
 ]]
-        reply_markup = InlineKeyboardMarkup(buttons)
+           reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply(script.START_TXT.format(message.from_user.mention if message.from_user else message.chat.title, temp.U_NAME, temp.B_NAME), reply_markup=reply_markup, disable_web_page_preview=True)
         await asyncio.sleep(2) # 😢 https://github.com/EvamariaTG/EvaMaria/blob/master/plugins/p_ttishow.py#L17 😬 wait a bit, before checking.
         if not await db.get_chat(message.chat.id):
