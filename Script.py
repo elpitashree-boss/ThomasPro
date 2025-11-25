fdc7224da6a1d31d68638cb31fcb5b375291cdde
elpitashree-boss <elpitashree@gmail.com>
2025-11-25 20:43:29 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -257,7 +257,7 @@ Nᴀᴍᴇ - {}"""
 <blockquote>‣ {file_name}</blockquote>
 <blockquote>━━━━━━━━━━━━━━</blockquote>
 <blockquote>‣ Uploaded by: <a href='http://t.me/EL_Pita_Shree'>𝔾𝖍𝖔𝖘𝖙 ♞</a></blockquote>
-<blockquote>‣ Join: <a href='http://t.me/Interworld_backup'>𝐈ɴᴛᴇʀᴡᴏʀʟᴅ ♞</a></blockquote>
+<blockquote>‣ Join: <a href='http://t.me/Dear_Batman'>𝐈ɴᴛᴇʀᴡᴏʀʟᴅ ♞</a></blockquote>
 <blockquote>‣ Size: {file_size}</blockquote>
 <blockquote>━━━━━━━━━━━━━━</blockquote>
 """ 
